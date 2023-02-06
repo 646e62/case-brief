e21c1ade144d28c95878e7cd5f1e6f9bd501dd22
Daniel Nathan Booy <danielnathanbooy@pop-os.localdomain>
2023-02-05 22:12:02 -0600

    First corpora complete with tokenization ability

---
+++ data/file_paths.py
@@ -0,0 +1,296 @@
+# Federal
+
+scc_list = ["./canlii_crim_corpus/html/2023/ca/scc/2023scc1.html",
+            "./canlii_crim_corpus/html/2023/ca/scc/2023scc2.html",
+            "./canlii_crim_corpus/html/2023/ca/scc/2023scc3.html",]
+
+# British Columbia
+
+bcca_list = ["./canlii_crim_corpus/html/2023/bc/ca/2023bcca2.html",
+             "./canlii_crim_corpus/html/2023/bc/ca/2023bcca3.html",
+             "./canlii_crim_corpus/html/2023/bc/ca/2023bcca4.html",
+             "./canlii_crim_corpus/html/2023/bc/ca/2023bcca6.html",
+             "./canlii_crim_corpus/html/2023/bc/ca/2023bcca8.html",
+             "./canlii_crim_corpus/html/2023/bc/ca/2023bcca13.html",
+             "./canlii_crim_corpus/html/2023/bc/ca/2023bcca16.html",
+             "./canlii_crim_corpus/html/2023/bc/ca/2023bcca19.html",
+             "./canlii_crim_corpus/html/2023/bc/ca/2023bcca29.html",
+             "./canlii_crim_corpus/html/2023/bc/ca/2023bcca33.html",
+             "./canlii_crim_corpus/html/2023/bc/ca/2023bcca37.html",
+             "./canlii_crim_corpus/html/2023/bc/ca/2023bcca38.html",
+             "./canlii_crim_corpus/html/2023/bc/ca/2023bcca50.html",]
+
+bcsc_list = ["./canlii_crim_corpus/html/2023/bc/sc/2023bcsc50.html",
+             "./canlii_crim_corpus/html/2023/bc/sc/2023bcsc147.html",
+             "./canlii_crim_corpus/html/2023/bc/sc/2023bcsc72.html",
+             "./canlii_crim_corpus/html/2023/bc/sc/2023bcsc85.html",
+             "./canlii_crim_corpus/html/2023/bc/sc/2023bcsc92.html",
+             "./canlii_crim_corpus/html/2023/bc/sc/2023bcsc96.html",
+             "./canlii_crim_corpus/html/2023/bc/sc/2023bcsc106.html",
+             "./canlii_crim_corpus/html/2023/bc/sc/2023bcsc134.html",
+             "./canlii_crim_corpus/html/2023/bc/sc/2023bcsc141.html",]
+
+bcpc_list = ["./canlii_crim_corpus/html/2023/bc/pc/2023bcpc3.html",
+             "./canlii_crim_corpus/html/2023/bc/pc/2023bcpc14.html",
+             "./canlii_crim_corpus/html/2023/bc/pc/2023bcpc4.html",
+             "./canlii_crim_corpus/html/2023/bc/pc/2023bcpc5.html",
+             "./canlii_crim_corpus/html/2023/bc/pc/2023bcpc6.html",
+             "./canlii_crim_corpus/html/2023/bc/pc/2023bcpc7.html",
+             "./canlii_crim_corpus/html/2023/bc/pc/2023bcpc11.html",
+             "./canlii_crim_corpus/html/2023/bc/pc/2023bcpc12.html",
+             "./canlii_crim_corpus/html/2023/bc/pc/2023bcpc13.html",]
+       
+    
+# Alberta
+
+abca_list = ["./canlii_crim_corpus/html/2023/ab/ca/2023abca2.html",
+             "./canlii_crim_corpus/html/2023/ab/ca/2023abca26.html",
+             "./canlii_crim_corpus/html/2023/ab/ca/2023abca3.html",
+             "./canlii_crim_corpus/html/2023/ab/ca/2023abca5.html",
+             "./canlii_crim_corpus/html/2023/ab/ca/2023abca7.html",
+             "./canlii_crim_corpus/html/2023/ab/ca/2023abca10.html",
+             "./canlii_crim_corpus/html/2023/ab/ca/2023abca11.html",
+             "./canlii_crim_corpus/html/2023/ab/ca/2023abca18.html",
+             "./canlii_crim_corpus/html/2023/ab/ca/2023abca20.html",
+             "./canlii_crim_corpus/html/2023/ab/ca/2023abca23.html",
+             "./canlii_crim_corpus/html/2023/ab/ca/2023abca29.html",]
+
+abkb_list = ["./canlii_crim_corpus/html/2023/ab/kb/2023abkb45.html",
+             "./canlii_crim_corpus/html/2023/ab/kb/2023abkb13.html",
+             "./canlii_crim_corpus/html/2023/ab/kb/2023abkb26.html",
+             "./canlii_crim_corpus/html/2023/ab/kb/2023abkb9.html",
+             "./canlii_crim_corpus/html/2023/ab/kb/2023abkb39.html",]
+
+abpc_list = ["./canlii_crim_corpus/html/2023/ab/pc/2023abpc17.html",
+             "./canlii_crim_corpus/html/2023/ab/pc/2023abpc22.html",
+             "./canlii_crim_corpus/html/2023/ab/pc/2023abpc3.html",
+             "./canlii_crim_corpus/html/2023/ab/pc/2023abpc9.html",
+             "./canlii_crim_corpus/html/2023/ab/pc/2023abpc6.html",
+             "./canlii_crim_corpus/html/2023/ab/pc/2023abpc1.html",
+             "./canlii_crim_corpus/html/2023/ab/pc/2023abpc16.html",
+             "./canlii_crim_corpus/html/2023/ab/pc/2023abpc8.html",
+             "./canlii_crim_corpus/html/2023/ab/pc/2023abpc7.html",]
+
+
+# Saskatchewan
+
+skca_list = ["./canlii_crim_corpus/html/2023/sk/ca/2023skca15.html",
+             "./canlii_crim_corpus/html/2023/sk/ca/2023skca1.html",
+             "./canlii_crim_corpus/html/2023/sk/ca/2023skca2.html",
+             "./canlii_crim_corpus/html/2023/sk/ca/2023skca12.html",
+             "./canlii_crim_corpus/html/2023/sk/ca/2023skca7.html",
+             "./canlii_crim_corpus/html/2023/sk/ca/2023skca6.html",]
+
+skkb_list = ["./canlii_crim_corpus/html/2023/sk/kb/2023skkb1.html",
+             "./canlii_crim_corpus/html/2023/sk/kb/2023skkb8.html",]
+
+skpc_list = ["./canlii_crim_corpus/html/2023/sk/pc/2023skpc6.html",
+             "./canlii_crim_corpus/html/2023/sk/pc/2023skpc10.html",
+             "./canlii_crim_corpus/html/2023/sk/pc/2023skpc2.html",
+             "./canlii_crim_corpus/html/2023/sk/pc/2023skpc9.html",
+             "./canlii_crim_corpus/html/2023/sk/pc/2023skpc5.html",
+             "./canlii_crim_corpus/html/2023/sk/pc/2023skpc14.html",
+             "./canlii_crim_corpus/html/2023/sk/pc/2023skpc1.html",
+             "./canlii_crim_corpus/html/2023/sk/pc/2023skpc3.html",
+             "./canlii_crim_corpus/html/2023/sk/pc/2023skpc4.html",
+             "./canlii_crim_corpus/html/2023/sk/pc/2023skpc12.html",
+             "./canlii_crim_corpus/html/2023/sk/pc/2023skpc8.html",
+             "./canlii_crim_corpus/html/2023/sk/pc/2023skpc7.html",]
+
+
+# Manitoba
+
+mbca_list = ["./canlii_crim_corpus/html/2023/mb/ca/2023mbca5.html",
+             "./canlii_crim_corpus/html/2023/mb/ca/2023mbca8.html",
+             "./canlii_crim_corpus/html/2023/mb/ca/2023mbca2.html",
+             "./canlii_crim_corpus/html/2023/mb/ca/2023mbca4.html",
+             "./canlii_crim_corpus/html/2023/mb/ca/2023mbca6.html",
+             "./canlii_crim_corpus/html/2023/mb/ca/2023mbca1.html",]
+
+mbkb_list = ["./canlii_crim_corpus/html/2023/mb/kb/2023mbkb7.html",
+             "./canlii_crim_corpus/html/2023/mb/kb/2023mbkb2.html",
+             "./canlii_crim_corpus/html/2023/mb/kb/2023mbkb10.html",
+             "./canlii_crim_corpus/html/2023/mb/kb/2023mbkb12.html",
+             "./canlii_crim_corpus/html/2023/mb/kb/2023mbkb6.html",
+             "./canlii_crim_corpus/html/2023/mb/kb/2023mbkb1.html",]
+
+mbpc_list = []
+
+
+# Ontario
+
+onca_list = ["./canlii_crim_corpus/html/2023/on/ca/2023onca19.html",
+             "./canlii_crim_corpus/html/2023/on/ca/2023onca33.html",
+             "./canlii_crim_corpus/html/2023/on/ca/2023onca40.html",
+             "./canlii_crim_corpus/html/2023/on/ca/2023onca23.html",
+             "./canlii_crim_corpus/html/2023/on/ca/2023onca2.html",
+             "./canlii_crim_corpus/html/2023/on/ca/2023onca24.html",
+             "./canlii_crim_corpus/html/2023/on/ca/2023onca45.html",
+             "./canlii_crim_corpus/html/2023/on/ca/2023onca6.html",
+             "./canlii_crim_corpus/html/2023/on/ca/2023onca10.html",
+             "./canlii_crim_corpus/html/2023/on/ca/2023onca38.html",
+             "./canlii_crim_corpus/html/2023/on/ca/2023onca13.html",
+             "./canlii_crim_corpus/html/2023/on/ca/2023onca48.html",
+             "./canlii_crim_corpus/html/2023/on/ca/2023onca5.html",
+             "./canlii_crim_corpus/html/2023/on/ca/2023onca35.html",
+             "./canlii_crim_corpus/html/2023/on/ca/2023onca8.html",
+             "./canlii_crim_corpus/html/2023/on/ca/2023onca31.html",
+             "./canlii_crim_corpus/html/2023/on/ca/2023onca32.html",
+             "./canlii_crim_corpus/html/2023/on/ca/2023onca47.html",
+             "./canlii_crim_corpus/html/2023/on/ca/2023onca53.html",
+             "./canlii_crim_corpus/html/2023/on/ca/2023onca7.html",
+             "./canlii_crim_corpus/html/2023/on/ca/2023onca20.html",
+             "./canlii_crim_corpus/html/2023/on/ca/2023onca12.html",
+             "./canlii_crim_corpus/html/2023/on/ca/2023onca3.html",
+             "./canlii_crim_corpus/html/2023/on/ca/2023onca36.html",
+             "./canlii_crim_corpus/html/2023/on/ca/2023onca4.html",]
+
+onsc_list = ["./canlii_crim_corpus/html/2023/on/sc/2023onsc538.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc414.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc124.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc496.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc103.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc286.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc547.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc254.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc347.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc549.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc62.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc396.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc64.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc283.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc452.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc640.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc220.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc14.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc97.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc268.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc662.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc568.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc621.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc462.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc146.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc296.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc555.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc190.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc200.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc416.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc166.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc567.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc400.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc300.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc519.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc406.html",
+             "./canlii_crim_corpus/html/2023/on/sc/2023onsc516.html",]
+
+oncj_list = ["./canlii_crim_corpus/html/2023/on/cj/2023oncj18.html",
+             "./canlii_crim_corpus/html/2023/on/cj/2023oncj24.html",
+             "./canlii_crim_corpus/html/2023/on/cj/2023oncj10.html",
+             "./canlii_crim_corpus/html/2023/on/cj/2023oncj9.html",
+             "./canlii_crim_corpus/html/2023/on/cj/2023oncj20.html",
+             "./canlii_crim_corpus/html/2023/on/cj/2023oncj25.html",
+             "./canlii_crim_corpus/html/2023/on/cj/2023oncj16.html",
+             "./canlii_crim_corpus/html/2023/on/cj/2023oncj12.html",
+             "./canlii_crim_corpus/html/2023/on/cj/2023oncj45.html",
+             "./canlii_crim_corpus/html/2023/on/cj/2023oncj43.html",
+             "./canlii_crim_corpus/html/2023/on/cj/2023oncj17.html",
+             "./canlii_crim_corpus/html/2023/on/cj/2023oncj28.html",
+             "./canlii_crim_corpus/html/2023/on/cj/2023oncj40.html",
+             "./canlii_crim_corpus/html/2023/on/cj/2023oncj6.html",
+             "./canlii_crim_corpus/html/2023/on/cj/2023oncj14.html",
+             "./canlii_crim_corpus/html/2023/on/cj/2023oncj36.html",
+             "./canlii_crim_corpus/html/2023/on/cj/2023oncj15.html",
+             "./canlii_crim_corpus/html/2023/on/cj/2023oncj11.html",
+             "./canlii_crim_corpus/html/2023/on/cj/2023oncj29.html",
+             "./canlii_crim_corpus/html/2023/on/cj/2023oncj5.html",
+             "./canlii_crim_corpus/html/2023/on/cj/2023oncj31.html",
+             "./canlii_crim_corpus/html/2023/on/cj/2023oncj27.html",
+             "./canlii_crim_corpus/html/2023/on/cj/2023oncj21.html",
+             "./canlii_crim_corpus/html/2023/on/cj/2023oncj4.html",
+             "./canlii_crim_corpus/html/2023/on/cj/2023oncj41.html",
+             "./canlii_crim_corpus/html/2023/on/cj/2023oncj22.html",]
+
+# Quebec
+
+qcca_list = ["./canlii_crim_corpus/html/2023/qc/ca/2023qcca34.html",
+             "./canlii_crim_corpus/html/2023/qc/ca/2023qcca13.html",
+             "./canlii_crim_corpus/html/2023/qc/ca/2023qcca89.html",
+             "./canlii_crim_corpus/html/2023/qc/ca/2023qcca57.html",]
+
+qccq_list = ["./canlii_crim_corpus/html/2023/qc/cq/2023qccq86.html",
+             "./canlii_crim_corpus/html/2023/qc/cq/2023qccq15.html",]
+
+qccs_list = []
+
+
+# New Brunswick
+
+nbca_list = ["./canlii_crim_corpus/html/2023/nb/ca/2023nbca6.html",]
+
+nbkb_list = []
+
+
+nbpc_list = ["./canlii_crim_corpus/html/2023/nb/pc/2023nbpc1.html",]
+
+# Newfoundland & Labrador
+
+nlsc_list = ["./canlii_crim_corpus/html/2023/nl/sc/2023nlsc6.html",]
+
+nlpc_list = ["./canlii_crim_corpus/html/2023/nl/pc/2023canlii605.html", 
+             "./canlii_crim_corpus/html/2023/nl/pc/2023canlii460.html",
+             "./canlii_crim_corpus/html/2023/nl/pc/2023canlii466.html",
+             "./canlii_crim_corpus/html/2023/nl/pc/2023canlii2060.html",
+             "./canlii_crim_corpus/html/2023/nl/pc/2023canlii2521.html",
+             "./canlii_crim_corpus/html/2023/nl/pc/2023canlii3051.html",]
+
+nlca_list = []
+
+# Prince Edward Island
+
+peca_list = ["./canlii_crim_corpus/html/2023/pe/ca/2023peca1.html",
+             "./canlii_crim_corpus/html/2023/pe/ca/2023peca2.html",]
+
+pesc_list = ["./canlii_crim_corpus/html/2023/pe/sc/2023peca4.html",]
+
+pepc_list = []
+
+
+# Nova Scotia
+
+nsca_list = ["./canlii_crim_corpus/html/2023/ns/ca/2023nsca3.html",
+             "./canlii_crim_corpus/html/2023/ns/ca/2023nsca2.html",
+             "./canlii_crim_corpus/html/2023/ns/ca/2023nsca1.html",]
+
+nssc_list = ["./canlii_crim_corpus/html/2023/ns/sc/2023nssc25.html",
+             "./canlii_crim_corpus/html/2023/ns/sc/2023nssc9.html",
+             "./canlii_crim_corpus/html/2023/ns/sc/2023nssc3.html",
+             "./canlii_crim_corpus/html/2023/ns/sc/2023nssc4.html",
+             "./canlii_crim_corpus/html/2023/ns/sc/2023nssc2.html",
+             "./canlii_crim_corpus/html/2023/ns/sc/2023nssc28.html",]
+
+nspc_list = []
+
+
+# Yukon
+
+ykca_list = []
+
+yksc_list = []
+
+yktc_list = ["./canlii_crim_corpus/html/2023/yk/pc/2023yktc1.html",]
+
+
+# Northwest Territories
+
+nwca_list = ["./canlii_crim_corpus/html/2023/nw/ca/2023nwca1.html",]
+
+nwsc_list = []
+
+nwtc_list = []
+
+
+# Nunavut
+
+nuca_list = []
+
+nucj_list = []
--- data/processing/html_to_text.py
+++ data/processing/html_to_text.py
@@ -1,5 +1,18 @@
-from bs4 import BeautifulSoup
+import spacy
 import re
+from bs4 import BeautifulSoup
+
+nlp = spacy.load("en_core_web_md")
+
+# Reads an HTML file and returns a BeautifulSoup object
+def read_html_file(filename: str)->BeautifulSoup:
+    '''
+    Reads an HTML file and returns a BeautifulSoup object.
+    '''
+    with open(filename, 'r', encoding="utf-8") as file:
+        soup: BeautifulSoup = BeautifulSoup(file, 'html.parser')
+    return soup
+
 
 def create_title(filepath: str)-> str:
     """Create a title for the text file from the html file name"""
@@ -7,53 +20,30 @@ def create_title(filepath: str)-> str:
     title_list = path_list[-1].split(".")
     title = title_list[0]
     
-    # title is a string composed of two groups of numbers and one group of
-    # letters. This function separes each group into a list.
-
     # The first group of numbers is the year
     year = re.findall(r"\d+", title)[0]
     # The second group of numbers is the file number
     file_number = re.findall(r"\d+", title)[1]
     # The group of letters is the jurisdiction and court
-    jurisdiction = re.findall(r"[A-Z]+", title)[0]
-
-    title = year + " " + jurisdiction.upper() + " " + file_number
+    jurisdiction = re.findall(r"[a-z]+", title)[0]
+    
+    if jurisdiction == "canlii":
+        jurisdiction = "CanLII"
+        title = f"{year} {jurisdiction} {file_number}"
+    else:
+        title = f"{year} {jurisdiction.upper()} {file_number}"
+ 
     return title
 
-def clean_text(paragraph: str, remove_para_nums=True: bool)->nltk.text:
-    '''
-    Returns tokenized text. The function can be set to include paragraph 
-    numbers for instances where they may provide some semantic value, but 
-    defaults to removing them as this generally isn't expected to be the case.
 
-    The function removes paragraphs by checking to see if the first character 
-    is a square bracket and if the next character is a number. If so, the 
-    function removes the opening bracket and the number, and then removes all
-    other characters until it reaches the closing bracket. If the first
-    character is not a square bracket, the function returns the paragraph
-    unchanged.
+def decision_paragraphs(filename: str)->tuple:
     '''
-    words = nltk.word_tokenize(paragraph)
-    
-    if remove_para_nums:
-        if paragraph[0] == "[" and paragraph[1].isdigit():
-            words = words[2:]
-            while words[0] != "]":
-                words = words[1:]
-            words = words[1:]
-
-    return words
-            
-
-def decision_paragraphs(filename: str)->list:
-    '''
-    Extracts the decision text from the numbered paragraphs. The decision text
+    Extracts the decision paragraphs. The decision text
     is contained in the <div class="paragWrapper"> tags. This function extracts
     the text from these tags and appends it to a list.
     '''
     
     decision = read_html_file(filename)
-    title = create_title(filename)
     
     # Find the first and last instances of the "paragWrapper" div
     first_div = decision.find("div", class_="paragWrapper")
@@ -64,22 +54,102 @@ def decision_paragraphs(filename: str)->list:
 
     # Iterate over all siblings between the first and last instances of the "paragWrapper" div
     sibling = first_div
-    paragraphs.append(title)
     paragraphs.append(first_div)
     while sibling != last_div:
         sibling = sibling.find_next_sibling()
         paragraphs.append(sibling)
         
-    # Find and append footnotes, where applicable
-    # Footnotes exist inside the "SPAN" tags with the class "MSoFootnoteReference"
-    # Not all decisions contain footnotes
+    # Finds and appends footnotes where applicable
     if decision.find("SPAN", class_="MsoFootnoteReference"):
-        footnote = decision.find("SPAN", class_="MsoFootnoteReference")
+        decision_footnotes(decision)
+        
+    return paragraphs, footnotes
+
+
+def decision_footnotes(decision: str)->list:
+    '''
+    Generates a list of footnotes in decisions containing them.
+    '''
+    footnote = decision.find("SPAN", class_="MsoFootnoteReference")
+    footnotes.append(footnote)
+    while footnote.find_next_sibling("SPAN", class_="MsoFootnoteReference"):
+        footnote = footnote.find_next_sibling("SPAN", class_="MsoFootnoteReference")
         footnotes.append(footnote)
-        while footnote.find_next_sibling("SPAN", class_="MsoFootnoteReference"):
-            footnote = footnote.find_next_sibling("SPAN", class_="MsoFootnoteReference")
-            footnotes.append(footnote)
-        paragraphs.append(footnotes)
+    
+    return footnotes
+
+
+def clean_text(paragraph: str, remove_para_nums: bool=True)->list:
+    '''
+    Returns text with problematic characters removed. These include paragraph
+    numbers enclosed in square brackets and superfluous periods after paragraph
+    and section pinpoints, as these can sometimes confuse the sentence 
+    detectors.
+    '''
+    doc = nlp(paragraph)
+    try:
+        if remove_para_nums and paragraph[0] == "[" and paragraph[1].isdigit():
+            doc = re.sub(r"\[\d+\]\s", "", paragraph)
+            return nlp(doc)
+        else:
+            return doc
+    except:
+        pass
+
+
+def export_to_file(clean_decision: list, filename: str):
+    '''
+    Saves a copy of the cleaned decision text to file.
+    '''
+    
+    file_path_list = filename.split("/")
+    del file_path_list[2]
+    file_path_list.insert(2,"txt")
+    save_path = "/".join(file_path_list)
+    
+    save_path_corrected = save_path.split(".")
+    del save_path_corrected[-1]
+    save_path_corrected.append("txt")
+    save_path_corrected = ".".join(save_path_corrected)
+    
+    with open(save_path_corrected, "w") as f:
+        #for item in clean_decision:
+            #f.write(str(item) + "\n")
         
+        for paragraph in clean_decision:
+            try:
+                f.write(paragraph + "\n")
+            except:
+                if paragraph:
+                    f.write(paragraph.text + "\n")
+    
+    print(save_path_corrected)
+
+
+def compile_decision_text(filename)->list:
+    '''
+    The aggregate function that runs the others.
+    '''
+    
+    decision = decision_paragraphs(filename)[0]
+    footnotes = decision_paragraphs(filename)[1]
+    clean_decision = []
+    clean_decision.append(filename)
+    
+    for paragraph in decision:
+        clean_decision.append(clean_text(paragraph.text))
+
+    for item in clean_decision:
+        try:
+            if len(item) == 0:
+                clean_decision.remove(item)
+        except:
+            pass
         
-    return paragraphs
+    if footnotes:
+        for footnote in footnotes:
+            decision.append(clean_text(footnote, False))
+    
+    return clean_decision
+
+
