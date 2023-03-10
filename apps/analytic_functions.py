f950d10505478948db4331159752df613c3ea6c9
Daniel Nathan Booy <danielnathanbooy@pop-os.localdomain>
2023-03-09 22:11:09 -0600

    New approach to the summarizer, reorganized the executable, trained a new NER model

--- apps/analytic_functions.py
+++ apps/analytic_functions.py
@@ -22,6 +22,22 @@ def retrieve_citations(text: str) -> dict:
         {"type": "legislation", "citations": [], "sections": []},
     ]
 
+    # Statutes that are likely to form part of a legal test
+    statutes = [
+        "Charter",
+        "Charter of Rights and Freedoms",
+        "Canadian Charter of Rights and Freedoms",
+        "Canadian Charter of Rights and Freedoms, Part I of the Constitution Act, 1982, being Schedule B to the Canada Act 1982 (UK), 1982, c 11",
+        "Canadian Charter of Rights and Freedoms, Part I of the Constitution Act, 1982",
+        "CDSA",
+        "Controlled Drugs and Substances Act",
+        "Controlled Drugs and Substances Act, SC 1996, c 19",
+        "Cr. C.",
+        "Cr C",
+        "Criminal Code",
+        "Criminal Code of Canada",
+        "Criminal Code, RSC 1985, c C-46",]
+
     # Extracting neutral and CanLII citations using regex was more economical 
     # (and probably accurate) than using the lightly-trained NLP model I used
     # previously.
@@ -29,22 +45,29 @@ def retrieve_citations(text: str) -> dict:
     citation_list = re.findall(case_citation_pattern, text)
     citations[0]["citations"] = citation_list
     
+    # Finds statutes from the list in the text and adds them to the statute
+    # list.
+    for statute in statutes:
+        if statute in text:
+            citations[1]["citations"].append(statute)
+
+    # Add the statutes to the list of citations 
+    citations[1]["citations"] = [statute for statute in statutes if statute in text]
+    
     # Although regex may be useful for extracting statute names, this will 
     # require a fairly comprehensive dictionary of all statutes in Canada that
     # are likely to appear in a legal test. Furthermore, regex turns out to be
     # a poor way to extract statute sections, given how variably they can be 
     # written, while a relatively simple NLP model can do a good job of getting
     # enough of them to make this function useful.
-    nlp = spacy.load("./models/ner_legislation_min_v1//model-last/")
+    nlp = spacy.load("./models/ner_sections_v1/model-last/")
     doc = nlp(text)
 
     for ent in doc.ents:
-        if ent.label_ == "STATUTE":
-            if ent.text in statute_list:
-                citations[1]["citations"].append(ent.text)
-        elif ent.label_ == "SECTION":
+#        if ent.label_ == "STATUTE":
+#            citations[1]["citations"].append(ent.text)
+        if ent.label_ == "SECTION":
             citations[1]["sections"].append(ent.text)
-               
     return citations
 
 
--- case_brief.py
+++ case_brief.py
@@ -8,6 +8,7 @@ import argparse
 import os.path
 import sys
 
+
 # from typing import List, Tuple
 
 from apps.analytic_functions import retrieve_citations, get_legal_test
@@ -15,89 +16,150 @@ from apps.classification_functions import classify_firac
 from apps.html_to_txt import canlii_html_to_txt
 from apps.summarization_functions import text_summarizer
 
-# Prompt the user for a file path
+
+# Argument functions
+
+def text_argument(file: str) -> str:
+    text = extract_text(file)
+    return text
+
+
+def local_argument(file: str, verbose: False) -> tuple:
+    text = extract_text(file)
+    firac = classify_firac(text)
+    citations = extract_citations(text)
+    summary = summarize_text_local(text)
+    analysis = analyze_text(text)
+
+    if verbose == True:
+        return text, firac, summary, analysis, citations
+    else:
+        return summary, analysis, citations
+
+
+def citation_argument():
+    text = extract_text(file)
+    return extract_citations(text)
+
+
+def default(file):
+    return local_argument(file)
+
+
+# Supportive functions
+
+def extract_text(file: str):
+    
+    print("Verifying file path: ", end = "")
+    if not os.path.exists(args.file_path):
+        print("File not found.")
+        sys.exit()
+    else:
+        print("Done.\n")
+   
+    # Write a local copy of the text file
+    file_name = os.path.basename(args.file_path)
+    file_name = os.path.splitext(file_name)[0]
+    file_name += ".txt"
+    file_path = f"./data/training_data/{file_name}"
+
+    # Convert the HTML file to text
+    text = canlii_html_to_txt(args.file_path)
+
+    # Check to see if a file copy exists. If not, create one.
+    if not os.path.exists(file_path):
+        with open(file_path, "w", encoding="utf-8") as file:
+            file.write(text)
+        print(f"Wrote {file_path}")
+    else:
+        print("File already exists.")
+
+
+def summarize_text_local(file: str, percentage: float = 0.2) -> tuple:
+    
+    for key in firac:
+        section_text = " ".join(firac[key])
+        section_text = text_summarizer(section_text, percentage)
+      
+    return key, section_text
+
+
+def extract_citations(text: str):
+    """
+    Extracts citations from a legal text, if any, and exports the results as a
+    dictionary.
+    """
+    print("Retrieving citations: ", end="")
+    citations = retrieve_citations(text)
+    print("Done")
+
+    decisions = []
+    legislation = []
+    sections = []
+ 
+    # Isolate the citations into a set
+    # If the underlying list is empty, a "None" string is added to the set
+    decisions = set(
+        [
+            decision
+            for citation in citations
+            if citation["type"] == "decisions"
+            for decision in citation["citations"]
+        ]
+    )
+
+    legislation = set(
+        [
+            statute
+            for citation in citations
+            if citation["type"] == "legislation"
+            for statute in citation["citations"]
+        ]
+    )
+
+    sections = set(
+        [
+            section
+            for citation in citations
+            if citation["type"] == "legislation"
+            for section in citation["sections"]
+        ]
+    )
+    
+    return decision_list
+
+def analyze_text_local(text: str):
+    # Check to see if any of the citations are in the legal tests
+    if get_legal_test(decision_list):
+
+        print("\nLegal test(s) found:")
+        for test in get_legal_test(decision_list):
+            print("* " + test["short_form"].title())
+    else:
+        print("\nNo legal tests found.")
+
+
+# Arguments
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "file_path",
+    nargs="?",
     help="The path to the HTML file to be processed.",
     type=str,
 )
-args = parser.parse_args()
-
-# Check if the file exists
-if not os.path.exists(args.file_path):
-    print("File not found.")
-    sys.exit()
-
-
-# File functions
-
-print("Retrieving citations: ", end="")
-text = canlii_html_to_txt(args.file_path)
-citations = retrieve_citations(text)
-firac = classify_firac(text)
-
-# Isolate the citations into a set
-# If the underlying list is empty, a "None" string is added to the set
-decision_list = []
-legislation_list = []
-section_list = []
-
-decision_list = set([
-    decision
-    for citation in citations
-    if citation["type"] == "decisions"
-    for decision in citation["citations"]
-])
-
-legislation_list = set([
-    statute
-    for citation in citations
-    if citation["type"] == "legislation"
-    for statute in citation["citations"]
-])
-
-section_list = set([
-    section
-    for citation in citations
-    if citation["type"] == "legislation"
-    for section in citation["sections"]
-])
-
-
-print(
-    f"\nDecisions:\n{decision_list}\n\nLegislation:\n{legislation_list}\n\nSections:\n{section_list}"
+parser.add_argument(
+    "--text",
+    nargs="?",
+    help="Converts the HTML file to text and exits."
 )
-
-for key in firac:
-    print(f"\n{key.upper()}: \n{firac[key]}")
-
-# Check to see if any of the citations are in the legal tests
-if get_legal_test(decision_list):
-    print("\nLegal test(s) found:")
-    # Find the "short_form" key for any legal tests that match the citation and
-    # print it in title case
-    for test in get_legal_test(decision_list):
-        print("* " + test["short_form"].title())
-else:
-    print("\nNo legal tests found.")
-
-# Take the list from each key and join them into a single string
-
-for key in firac:
-    print(key.upper)
-    section_text = " ".join(firac[key])
-    section_text = text_summarizer(section_text, 0.5)
-    print(f"\n{key.upper()}: \n{section_text}")
-
-# Write a local copy of the text file
-# Take the HTML file name and change the extension to .txt
-file_name = os.path.basename(args.file_path)
-file_name = os.path.splitext(file_name)[0]
-file_name = file_name + ".txt"
-
-# Check to see if a file copy exists. If not, create one.
-if not os.path.exists(f"./data/training_data/{file_name}"):
-    with open("./data/training_data/" + file_name, "w", encoding="utf-8") as file:
-        file.write(text)
+parser.add_argument(
+    "--local",
+    nargs="?",
+    help="Analyzes a file without calling the GPT-3.5 API."
+)
+parser.add_argument("--citation", 
+    nargs="?", 
+    help="Extracts citations and exits."
+)
+args = parser.parse_args()
 
